f9b6bd5ea442103d8333ce9f08474c677e5f0368
Jacky Wu <JackyWuCoder@gmail.com>
2024-12-18 02:44:26 -0800

    apply_impulse Function
    1) Class Function of RigidBody2D.
    2) Applies a positioned impulse relative to the body origin in global coordinates.
    3) This impulse is time-independent and should be a one time use b/c applying this impulse within every frame would cause framerate-dependent force.
    4) When simulating multi-impace forces we should use _force functions instead of _impulse functions.

--- player.gd
+++ player.gd
@@ -3,9 +3,4 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	apply_impulse(Vector2(-25, 0))
