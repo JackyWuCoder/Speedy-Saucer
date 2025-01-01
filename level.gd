16f872ebe7d3de383431dfa2c44152fe375252e9
Jacky Wu <JackyWuCoder@gmail.com>
2025-01-01 06:01:24 -0800

    Code Cleanup
    1) Remove testing code

--- level.gd
+++ level.gd
@@ -1,13 +1,4 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("Hello, world!")
-	
 func _on_maze_body_exited(body):
 	get_tree().reload_current_scene();
-
-
-func _on_maze_body_entered(body: Node2D) -> void:
-	print("body in")
--- player.gd
+++ player.gd
@@ -1,10 +1,8 @@
 extends RigidBody2D
 
-func _ready():
-	print(add(10, 5))
+var force = 1500
 
 func _physics_process(delta):
-	var force = 1000
 	if Input.is_action_pressed("move_right"):
 		apply_force(Vector2(force, 0))
 	if Input.is_action_pressed("move_left"):
@@ -13,10 +11,3 @@ func _physics_process(delta):
 		apply_force(Vector2(0, -force))
 	if Input.is_action_pressed("move_down"):
 		apply_force(Vector2(0, force)) 
-
-func test():
-	var t = 10
-	return (t+10)
-	
-func add(num1, num2):
-	return num1 + num2
